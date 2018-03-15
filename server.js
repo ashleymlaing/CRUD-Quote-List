9454c3c172b6e8235a18ee8e9920315cf380bcba
Ashley Marie Laing <ashleym.laing@gmail.com>
2018-03-14 22:26:12 -0400

    having errors with mongodb and mlab, getting an error: Error: listen EADDRINUSE :::3000

--- server.js
+++ server.js
@@ -6,14 +6,6 @@ var db
 
 const MongoClient = require('mongodb').MongoClient
 
-MongoClient.connect('link-to-mongodb', (err, database) => {
-    // ... start the server
-})
-
-MongoClient.connect('mongodb://<dbuser>:<dbpassword>@ds213209.mlab.com:13209/quotes-crud', (err, client) => {
-    // ... do something here
-})
-
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(3000, function() {
@@ -33,3 +25,37 @@ app.post('/quotes', (req, res) => {
 })
 
 // console.log('May Node be with you')
+
+MongoClient.connect('mongodb://alaing:noel1129@ds213209.mlab.com:13209/quotes-crud', (err, client) => {
+    // ... start the server
+
+    if (err) return console.log(err)
+    db = client.db('quotes-crud')
+
+    app.listen(3000, () => {
+        console.log('listening on 3000')
+    })
+})
+
+// MongoClient.connect('mongodb://<dbuser>:<dbpassword>@ds213209.mlab.com:13209/quotes-crud', (err, client) => {
+//     // ... do something here
+// })
+
+app.post('/quotes', (req, res) => {
+    db.collection('quotes').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+
+
+        console.log('saved to database')
+        res.redirect('/')
+    })
+})
+
+// app.get('/', (req, res) => {
+//     var cursor = db.collection('quotes').find()
+// })
+
+// db.collection('quotes').find().toArray(function(err, results) {
+//     console.log(results)
+//         // send HTML file populated with quotes here
+// })
