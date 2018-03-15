5992357338d9762686e67dfd3e61baae30ce5bae
Ashley Marie Laing <ashleym.laing@gmail.com>
2018-03-15 16:39:13 -0400

    got the mongodb connected, now trying to pull quotes from DB

--- server.js
+++ server.js
@@ -8,10 +8,6 @@ const MongoClient = require('mongodb').MongoClient
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.listen(3000, function() {
-    console.log('listening on 3000')
-})
-
 app.get('/', (req, res) => {
 
     res.sendFile(__dirname + '/index.html')
@@ -20,42 +16,31 @@ app.get('/', (req, res) => {
 
 })
 
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
-})
+// app.post('/quotes', (req, res) => {
+//     console.log("Quotes added : ", req.body)
+// })
 
 // console.log('May Node be with you')
 
-MongoClient.connect('mongodb://alaing:noel1129@ds213209.mlab.com:13209/quotes-crud', (err, client) => {
-    // ... start the server
+MongoClient.connect('mongodb://alaing:Noel1129@ds139984.mlab.com:39984/crud-quotes', (err, client) => {
 
     if (err) return console.log(err)
-    db = client.db('quotes-crud')
+    db = client.db('crud-quotes')
 
     app.listen(3000, () => {
         console.log('listening on 3000')
     })
 })
 
-// MongoClient.connect('mongodb://<dbuser>:<dbpassword>@ds213209.mlab.com:13209/quotes-crud', (err, client) => {
-//     // ... do something here
-// })
-
 app.post('/quotes', (req, res) => {
     db.collection('quotes').save(req.body, (err, result) => {
         if (err) return console.log(err)
-
 
         console.log('saved to database')
         res.redirect('/')
     })
 })
 
-// app.get('/', (req, res) => {
-//     var cursor = db.collection('quotes').find()
-// })
-
-// db.collection('quotes').find().toArray(function(err, results) {
-//     console.log(results)
-//         // send HTML file populated with quotes here
-// })
+app.get('/', (req, res) => {
+    var cursor = db.collection('quotes').find()
+})
